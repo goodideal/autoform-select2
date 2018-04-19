f36606ed15758a5b1a01d0d6fdfe95a0c4a1e11c
Jerry <jerry@xqopen.cn>
2018-04-19 13:23:54 +0800

    first commit

---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  name: 'goodideal:autoform-select2',
+  summary: 'Custom select2 input type for AutoForm - modified by goodideal',
+  version: '3.0.0',
+  //git: 'https://github.com/aldeed/meteor-autoform-bs-datetimepicker.git'
+});
+
+Package.onUse(function(api) {
+  api.use('meteor');
+  api.use('underscore@1.0.0');
+  api.use('templating@1.0.0');
+  api.use('blaze@2.0.0');
+  api.use('aldeed:template-extension@4.0.0');
+  api.use('aldeed:autoform@6.0.0');
+
+  api.addFiles([
+    'autoform-select2.html',
+    'autoform-select2.js'
+  ], 'client');
+});
